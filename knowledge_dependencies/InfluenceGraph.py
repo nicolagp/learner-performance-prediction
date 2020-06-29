8064aa9551917ae581520480e44525345c60720a
nicolagp <nicolagp@usc.edu>
2020-06-29 13:33:46 -0300

    removed old files

--- knowledge_dependencies/InfluenceGraph.py
+++ knowledge_dependencies/InfluenceGraph.py
@@ -3,24 +3,24 @@ import matplotlib.pyplot as plt
 
 class InfluenceGraph():
 
-def __init__(self, adj_matrix=None, path=None, graph=None):
-        """
-        - Initialize class from and adjacency matrix, a .edges file or a
-        nx.DiGraph object
-        - Throws exception if none or more than one are provided
-        """
-        if path is None and adj_matrix is None and graph is None:
-            raise AttributeError("No source provided to build graph.")
-        elif (path is not None and adj_matrix is not None) or (path is not None and graph is not None) or(adj_matrix is not None and graph is not None):
-            raise AttributeError("Too many sources provided to build graph.")
-        elif adj_matrix is not None:
-            self.graph = nx.to_networkx_graph(adj_matrix, create_using=nx.DiGraph)
-        elif path is not None:
-            self.graph = nx.read_edgelist("g_removed_by_mfas.edges",
-                                          create_using = nx.DiGraph(),
-                                          nodetype = int)
-        else:
-            self.graph = graph
+    def __init__(self, adj_matrix=None, path=None, graph=None):
+            """
+            - Initialize class from and adjacency matrix, a .edges file or a
+            nx.DiGraph object
+            - Throws exception if none or more than one are provided
+            """
+            if path is None and adj_matrix is None and graph is None:
+                raise AttributeError("No source provided to build graph.")
+            elif (path is not None and adj_matrix is not None) or (path is not None and graph is not None) or(adj_matrix is not None and graph is not None):
+                raise AttributeError("Too many sources provided to build graph.")
+            elif adj_matrix is not None:
+                self.graph = nx.to_networkx_graph(adj_matrix, create_using=nx.DiGraph)
+            elif path is not None:
+                self.graph = nx.read_edgelist("g_removed_by_mfas.edges",
+                                            create_using = nx.DiGraph(),
+                                            nodetype = int)
+            else:
+                self.graph = graph
 
 
     def save_edges(self, path):
